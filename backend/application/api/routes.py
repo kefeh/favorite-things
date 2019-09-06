cbca81e1959100a789e81dd00a34a5da54cbb087
kefeh <kefehcollins@gmail.com>
2019-09-06 07:30:38 +0100

    Type: Fix

--- backend/application/api/routes.py
+++ backend/application/api/routes.py
@@ -1,8 +1,34 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+from models import *
 
 print(__name__)
 test = Blueprint('test', __name__)
 
-@test.route('/')
+@test.route('/add/category', methods=['POST'])
 def hello_world():
-    return 'Hello World'
+    data = request.get_json()
+    title = data.get('title')
+    desc = data.get('description')
+
+    category = Category(title=title, description=desc)
+    try:
+        db.session.add(category)
+        db.session.commit()
+        return jsonify(data={'result': 'success'})
+    except Exception as exp:
+        print(exp)
+        return jsonify(data={'result': 'Failure'})
+
+@test.route('/remove/category', methods=['DELETE'])
+def remove_world():
+    category = db.session.query(Category).filter(Category.title == "Another").one()
+    db.session.delete(category)
+    db.session.commit()
+    return jsonify(results={'value': "success"})
+
+@test.route('/remove/category', methods=['PUT'])
+def update_world():
+    category = db.session.query(Category).filter(Category.title == "Another").one()
+    category.title = "OhGod"
+    db.session.commit()
+    return jsonify(results={'value': "success"})
--- backend/models.py
+++ backend/models.py
@@ -2,37 +2,67 @@ from sqlalchemy import event
 from application import db
 from datetime import datetime
 
+
+def get_doc_info(data):
+    return {
+        'table_name': data.__tablename__,
+        'doc_id': data.id,
+        'doc_name': data.title,
+    }
+
+
+def save_data(data):
+    db.session.add(data)
+    try:
+        db.session.commit()
+    except Exception as exp:
+        print(exp)
+        db.session.rollback()
+        raise Exception('Unable to save to db')
+
+
+def get_modified_fields(data):
+    """returns a list of fieldChange Objects, each of this Object will have the field_name, the old value and the new value"""
+
+    from sqlalchemy.orm.attributes import get_history
+    fields: list = []
+    print(data)
+    for field in data:
+        change = get_history(data, field)
+        if change.has_changes():
+            old = change.deleted[0]
+            new = change.added[0]
+            print(field, old, new)
+
+
 class LoggableMixin:
 
     @staticmethod
     def log_after_insert(mapper, connection, target):
         #do some stuff for the insert
-        from pprint import pprint
-        print("Inserted ##########################################")
-        pprint(target)
-        pprint(connection)
-        pprint(mapper)
-        print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
+        data = get_doc_info(target)
+        connection.execute(
+            AuditLog.__table__.insert(), log_group=data.get('table_name'), log_item=data.get('doc_name'), log_id=data.get('doc_id'), log_action='insert'
+        )
+        log = AuditLog(log_group=data.get('table_name'), log_item=data.get('doc_name'), log_id=data.get('doc_id'), log_action='insert')
+        # save_data(log)
+        print(log)
 
     @staticmethod
     def log_after_update(mapper, connection, target):
         #do some stuff for the insert, maybe saving the changed fields values using get_history
-        from pprint import pprint
-        print('Updated ############################################')
-        pprint(target)
-        pprint(connection)
-        pprint(mapper)
-        print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
+        get_modified_fields(target)
     
     @staticmethod
     def log_after_delete(mapper, connection, target):
         #do some stuff after deletion
-        from pprint import pprint
-        print('Deleted #############################################')
-        pprint(target)
-        pprint(connection)
-        pprint(mapper)
-        print('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@')
+        data = get_doc_info(target)
+        connection.execute(
+            AuditLog.__table__.insert(), log_group=data.get('table_name'), log_item=data.get('doc_name'), log_id=data.get('doc_id'), log_action='delete'
+        )
+        log = AuditLog(log_group=data.get('table_name'), log_item=data.get('doc_name'), log_id=data.get('doc_id'), log_action='delete')
+        # save_data(log)
+        print(log)
 
     @classmethod
     def __declare_last__(cls):
@@ -53,7 +83,8 @@ class Category(LoggableMixin, db.Model):
     favorite_things = db.relationship('FavoriteThing', backref='categories',  lazy=True)
 
     def __repr__(self):
-        return f"Category(Title:'{self.title}', '{self.description}', Created_at:'{self.createdAt}')"
+        return f"Category(Title:'{self.title}', '{self.description}', Created_at:'{self.created_at}')"
+
 
 class FavoriteThing(LoggableMixin, db.Model):
 
@@ -67,12 +98,14 @@ class FavoriteThing(LoggableMixin, db.Model):
     modified_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow())
 
     def __repr__(self):
-        return f"Favorite Thing (Title:'{self.title}', '{self.description}', Category: '{self.categories.title}' Created_at:'{self.createdAt}')"
+        return f"Favorite Thing (Title:'{self.title}', '{self.description}', Category: '{self.categories.title}' Created_at:'{self.created_at}')"
 
 
 class AuditLog(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow(), doc="Time logged or time of transaction")
-    log_name = db.Column(db.String(50), nullable=False, doc="Name of the logged field")
+    log_group = db.Column(db.String(50), nullable=False, doc="Name of the logged Class")
+    log_item = db.Column(db.String(50), nullable=False, doc="Name of the logged item")
     log_id = db.Column(db.Integer, nullable=False)
+    log_action = db.Column(db.String(50), nullable=False, doc="Name of the action that triggered the log")
